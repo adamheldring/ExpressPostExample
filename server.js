60b0ff7c5471cfa080264af0dde842ce55bda9ef
A-Game <javascript@adamheldring.com>
2018-11-01 10:37:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+ import mongoose from "mongoose"
 import express from "express"
 import bodyParser from "body-parser"
 
@@ -19,7 +19,10 @@ mongoose.Promise = Promise
 // })
 
 const Fruit = mongoose.model("Fruit", {
-  name: String,
+  name: {
+    type: String,
+    enum: ["Apple", "Pear"]
+  },
   isSour: Boolean,
   rating: Number
 })
@@ -34,6 +37,29 @@ app.get("/fruits", (req, res) => {
     res.json(fruits)
   })
 })
+
+app.post("/fruits", (req, res) => {
+    console.log(req.body)
+    const newFruit = new Fruit({
+      name: req.body.name,
+      isSour: req.body.isSour,
+      rating: req.body.rating
+    })
+    newFruit.save()
+      .then(() => {
+        res.status(201).json({ created: true })
+      })
+      .catch((err) => {
+        res.status(400).json({ created: false, error: err })
+      })
+})
+
+
+// THIS IS ANOTHER WAY WITH LESS CODE,
+// JUST PASSING IN THE ENTIRE OBJECT, BUT FOR SECURITY YOUR SHOULD
+// ALWAYS CHECK AND VALIDATE THE OBJECT FIRST TO MAKE SURE NO EVIL CODE
+// IS INJECTED
+// const newFruit = new Fruit(req.body)
 
 app.listen(8080, () =>
   console.log("Example app listening on port 8080!")
